d48e9008cbf8f0ec4046d7bfd8b47242881acf8f
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-08-21 17:03:06 +0200

    feat:create skill atom and skill capsule tables

---
+++ src/main/java/com/capstone/skill_service/model/CapsuleAtomMappingEntity.java
@@ -0,0 +1,33 @@
+package com.capstone.skill_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "capsule_atoms")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CapsuleAtomMappingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "skill_capsule_id")
+    private SkillCapsuleEntity capsule;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "skill_atom_id")
+    private SkillAtomEntity atom;
+
+    @Column(nullable = false)
+    private Integer sequenceOrder;
+
+}
---
+++ src/main/java/com/capstone/skill_service/model/SkillCapsuleEntity.java
@@ -0,0 +1,47 @@
+package com.capstone.skill_service.model;
+
+import com.capstone.skill_service.util.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "skill_capsules")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SkillCapsuleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String category_type;
+    private String difficulty;
+    private int proficiency_level;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(mappedBy = "capsule", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CapsuleAtomMappingEntity> skillAtoms = new ArrayList<>();
+
+    private int moodle_course_id; // id from moodle database on course table
+    private int estimated_hours;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
