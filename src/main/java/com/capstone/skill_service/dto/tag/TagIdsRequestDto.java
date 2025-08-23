a4a24b65892f4a1e624f2026364eb73914e6b9f3
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-08-23 21:01:39 +0200

    feat: remove tag from skill capsule

---
+++ src/main/java/com/capstone/skill_service/dto/tag/TagIdsRequestDto.java
@@ -0,0 +1,18 @@
+package com.capstone.skill_service.dto.tag;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TagIdsRequestDto {
+    private List<UUID> tagIds;
+}
