8a910abfd94c40ea5c65ca89a39a014435b32701
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-08-24 11:56:09 +0200

    perf: fetch with pagination using two-step approach
    To prevent the MultipleBagFetchException with pagination, this approach
    is used to keep the pagination consistent.

---
+++ src/main/java/com/capstone/skill_service/repository/CapsuleAtomMappingRepository.java
@@ -0,0 +1,25 @@
+package com.capstone.skill_service.repository;
+
+import com.capstone.skill_service.model.CapsuleAtomMappingEntity;
+import com.capstone.skill_service.model.SkillAtomEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CapsuleAtomMappingRepository extends JpaRepository<CapsuleAtomMappingEntity, UUID> {
+    @Query("""
+    SELECT cam
+    FROM CapsuleAtomMappingEntity cam
+    JOIN FETCH cam.atom
+    WHERE cam.capsule.id IN :capsuleIds
+    ORDER BY cam.capsule.id, cam.sequenceOrder
+""")
+    List<CapsuleAtomMappingEntity> findByCapsuleIdsWithAtoms(@Param("capsuleIds") List<UUID> capsuleIds);
+
+}
--- src/main/java/com/capstone/skill_service/repository/CapsuleRepository.java
+++ src/main/java/com/capstone/skill_service/repository/CapsuleRepository.java
@@ -1,13 +1,17 @@
 package com.capstone.skill_service.repository;
 
+import com.capstone.skill_service.model.CapsuleAtomMappingEntity;
 import com.capstone.skill_service.model.SkillCapsuleEntity;
+import com.capstone.skill_service.model.TagEntity;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -32,4 +36,15 @@ public interface CapsuleRepository extends JpaRepository<SkillCapsuleEntity, UUI
         WHERE c.id = :id
     """)
     Optional<SkillCapsuleEntity> findByIdWithSkillAtoms(@Param("id") UUID id);
+
+    /* to fetch capsule with its atom, tag */
+
+    // capsule with pagination
+    @Query("SELECT c.id FROM SkillCapsuleEntity c ORDER BY c.createdAt DESC")
+    Page<UUID> findPagedIds(Pageable pageable);
+
+    //fetch capsule only
+    @Query("SELECT c FROM SkillCapsuleEntity c WHERE c.id IN :ids")
+    List<SkillCapsuleEntity> findByIdIn(@Param("ids") List<UUID> ids);
+
 }
