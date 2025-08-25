f44b053b2731f4b6502a429a23f81e8a3623b102
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-08-25 10:02:04 +0200

    feat: create growth track entity and dtos

---
+++ src/main/java/com/capstone/skill_service/mapper/TrackMapper.java
@@ -0,0 +1,19 @@
+package com.capstone.skill_service.mapper;
+
+import com.capstone.skill_service.dto.track.TrackRequestDto;
+import com.capstone.skill_service.dto.track.TrackResponseDto;
+import com.capstone.skill_service.dto.track.TrackWithCapsuleResponseDto;
+import com.capstone.skill_service.model.GrowthTrackEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface TrackMapper {
+    @Mapping(target = "capsules",  source = "capsules")
+    TrackResponseDto toDto(GrowthTrackEntity entity);
+
+    GrowthTrackEntity toEntity(TrackRequestDto dto);
+    @Mapping(target = "capsules",  ignore = true)
+    TrackWithCapsuleResponseDto toWithCapsuleDto(GrowthTrackEntity entity);
+
+}
