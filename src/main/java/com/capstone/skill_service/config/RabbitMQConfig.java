a8b743b4d2c792ca940749285144e78cfd374495
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-08 18:25:29 +0200

    feat: configure RabbitMQ for internal communication

---
+++ src/main/java/com/capstone/skill_service/config/RabbitMQConfig.java
@@ -0,0 +1,30 @@
+package com.capstone.skill_service.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+
+    @Bean
+    public Queue createSkillQueue() {
+        return new Queue("versapath.skill.create", true);
+    }
+}
