2903363043f92a36a45149254bf9294d4c9ab164
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-09 22:20:18 +0200

    feat: populate skill atom event after insertion in DB

--- src/main/java/com/capstone/skill_service/messaging/PopulateSkillEvents.java
+++ src/main/java/com/capstone/skill_service/messaging/PopulateSkillEvents.java
@@ -1,16 +1,20 @@
 package com.capstone.skill_service.messaging;
 
+import com.capstone.skill_service.service.impl.AtomServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.common.event.GrowthTrackEvent;
 import org.common.event.SkillAtomEvent;
 import org.common.event.SkillCapsuleEvent;
 import org.common.event.TalentRouteEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class PopulateSkillEvents {
+    private static final Logger logger = LoggerFactory.getLogger(PopulateSkillEvents.class);
     private final KafkaTemplate<String, SkillAtomEvent> kafkaAtomTemplate;
     private final KafkaTemplate<String, SkillCapsuleEvent> kafkaCapsuleTemplate;
     private final KafkaTemplate<String, GrowthTrackEvent> kafkaGrowthTrackTemplate;
@@ -18,6 +22,8 @@ public class PopulateSkillEvents {
 
     public void populateSkillAtom(SkillAtomEvent atomEvent){
         kafkaAtomTemplate.send("atom.create", atomEvent);
+
+        logger.info("atom event is populated: {}", atomEvent);
     }
 
     public void populateSkillCapsule(SkillCapsuleEvent capsuleEvent){
