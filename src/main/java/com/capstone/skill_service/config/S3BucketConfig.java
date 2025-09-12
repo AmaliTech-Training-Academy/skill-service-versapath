74dfa751090523e48db13bb004ad4fa3640f1d57
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-12 10:08:21 +0200

    feat: configure S3 bucket to upload file

---
+++ src/main/java/com/capstone/skill_service/config/S3BucketConfig.java
@@ -0,0 +1,31 @@
+package com.capstone.skill_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import org.springframework.beans.factory.annotation.Value;
+
+@Component
+public class S3BucketConfig {
+    @Value("${AWS_ACCESS_KEY_ID}")
+    private String accessKey;
+
+    @Value("${AWS_SECRET_ACCESS_KEY}")
+    private String secretKey;
+
+    @Value("${AWS_REGION}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey))
+                )
+                .build();
+    }
+}
